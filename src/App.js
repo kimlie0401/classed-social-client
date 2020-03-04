4ea403b2866783d2edb4299ab5022ac5bf23e767
Dong kun Kim <dk@Dongs-MacBook-Air.local>
2020-03-03 23:07:46 -0800

    #15 Scream Card Details

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 
 // Components
 import Navbar from "./components/Navbar";
@@ -10,20 +12,39 @@ import home from "./pages/home";
 import login from "./pages/login";
 import signup from "./pages/signup";
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#546e7a",
+      main: "#455a64",
+      dark: "#37474f",
+      contrastText: "#fff"
+    },
+    secondary: {
+      light: "#efebe9",
+      main: "#d7ccc8",
+      dark: "#bcaaa4",
+      contrastText: "#000"
+    }
+  }
+});
+
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={home} />
-            <Route exact path="/login" component={login} />
-            <Route exact path="/signup" component={signup} />
-          </Switch>
-        </div>
-      </Router>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={home} />
+              <Route exact path="/login" component={login} />
+              <Route exact path="/signup" component={signup} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,39 @@
 import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import axios from "axios";
 
 class home extends Component {
+  state = {
+    screams: null
+  };
+
+  getScreams = async () => {
+    await axios
+      .get("/screams")
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          screams: res.data
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+  componentDidMount = () => {
+    this.getScreams();
+  };
+
   render() {
     return (
-      <div>
-        <h1>Home Page</h1>
-      </div>
+      <Grid container spacing={2}>
+        <Grid item sm={8} xs={12}>
+          <p>Content...</p>
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>Profile...</p>
+        </Grid>
+      </Grid>
     );
   }
 }
