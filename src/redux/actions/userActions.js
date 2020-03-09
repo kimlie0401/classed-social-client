841249c67512e4c23650bce13546bf75570939be
kimlie0401 <54561489+kimlie0401@users.noreply.github.com>
2020-03-09 09:41:31 -0700

    #21 Image Upload

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -68,6 +68,16 @@ export const getUserData = () => dispatch => {
     .catch(err => console.log(err));
 };
 
+export const uploadImage = formData => dispatch => {
+  dispatch({ type: LOADING_USER });
+  axios
+    .post("/user/image", formData)
+    .then(() => {
+      dispatch(getUserData());
+    })
+    .catch(err => console.log(err));
+};
+
 const setAuthorizationHeader = token => {
   const FBIdToken = `Bearer ${token}`;
   let expireTime = 1 / 24; // 1 hour
