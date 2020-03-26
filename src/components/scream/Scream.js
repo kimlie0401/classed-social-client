8dbedd4812c1236a688429f2278f11eefc72177b
kimlie0401 <54561489+kimlie0401@users.noreply.github.com>
2020-03-26 16:40:59 -0700

    #32 Notifications

--- src/components/scream/Scream.js
+++ src/components/scream/Scream.js
@@ -106,7 +106,11 @@ class Scream extends Component {
             <ChatIcon color="primary" />
           </MyButton>
           <span>{commentCount} comments</span>
-          <ScreamDialog screamId={screamId} userHandle={userHandle} />
+          <ScreamDialog
+            screamId={screamId}
+            userHandle={userHandle}
+            openDialog={this.props.openDialog}
+          />
           {editButton}
           {deleteButton}
         </CardActions>
@@ -118,7 +122,8 @@ class Scream extends Component {
 Scream.propTypes = {
   user: PropTypes.object.isRequired,
   scream: PropTypes.object.isRequired,
-  classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
+  openDialog: PropTypes.bool
 };
 
 const mapStateToProps = state => ({
